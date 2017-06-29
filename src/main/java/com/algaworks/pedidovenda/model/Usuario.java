91d92d66e06c9c2b9013e95770ea3752b5178991
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2017-06-29 13:31:16 -0300

    mudando o usuario

--- src/main/java/com/algaworks/pedidovenda/model/Usuario.java
+++ src/main/java/com/algaworks/pedidovenda/model/Usuario.java
@@ -1,17 +1,11 @@
 package com.algaworks.pedidovenda.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
